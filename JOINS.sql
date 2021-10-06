420cd47592a4d5fc77116f01cdbafe152e69517f
shene <sheneboshoff6@gmail.com>
2021-10-06 09:47:27 +0200

    Added Joins

--- JOINS.sql
+++ JOINS.sql
@@ -11,9 +11,30 @@ NATURAL JOIN Insurance
 WHERE Insurance_Name = 'Old Mutual';
 
 --Fire events by date (Asc or Desc)
+SELECT FIRE_EVENT.Event_Name, FIRE_LGA.Event_Date
+FROM FIRE_EVENT
+INNER JOIN FIRE_LGA ON FIRE_LGA.Event_ID = FIRE_EVENT.Event_ID
+ORDER BY FIRE_LGA.Event_Date;
+
 --Fire events by date (period to period)
+SELECT FIRE_EVENT.Event_Name, FIRE_LGA.Event_Date
+FROM FIRE_EVENT
+INNER JOIN FIRE_LGA on FIRE_LGA.Event_ID = FIRE_EVENT.Event_ID
+WHERE FIRE_LGA.Event_Date BETWEEN '01-APR-21' AND '31-AUG-21'
+ORDER BY FIRE_LGA.Event_Date;
+
 --Number of fire events per LGA
+SELECT LGA.Lga_Name, COUNT(FIRE_LGA.LGA_Code) AS "Number of fire events"
+FROM LGA
+INNER JOIN FIRE_LGA ON LGA.LGA_Code = FIRE_LGA.LGA_Code
+GROUP BY LGA.LGA_Name;
+
 --Biggest fire event per LGA
+SELECT LGA.LGA_Name, FIRE_EVENT.Event_Name, MAX(FIRE_EVENT.Event_Areasize) AS "Area Size"
+FROM LGA
+INNER JOIN FIRE_LGA ON LGA.LGA_Code = FIRE_EVENT.Event_ID
+INNER JOIN FIRE_EVENT ON FIRE_LGA.Event_ID = FIRE_EVENT.Event_ID
+GROUP BY LGA.LGA_Name, FIRE_EVENT.Event_Name;
 
 --Highest body count per LGA
 --List of owners by type
